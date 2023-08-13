ab18aabfefd8e2f2ef3907832c4ffd6a2f512fb5
Harikb <harikrishnan.b@inapp.com>
2023-08-13 15:29:47 +0530

    Final for now

--- api/main.py
+++ api/main.py
@@ -16,4 +16,4 @@ load_dotenv('.env')
 
 if __name__ == "__main__":
     # Run uvicorn server
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, workers=2) # reload=True
